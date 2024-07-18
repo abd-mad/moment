2cf948fb34c940a38da4dc244a72985d09628fd1
abd-mad <145804522+abd-mad@users.noreply.github.com>
2024-07-18 16:47:42 +0300

    Add files via upload

---
+++ js/scrpit.js
@@ -0,0 +1,20 @@
+// let num =prompt('Enter the marke')
+ 
+// if(num> 90 && num <100 ){
+//      console .log ("A")}
+
+//     else if(num> 80 && num <90 ){
+//         console .log ("B")}
+
+//     else if(num> 70 && num <80 ){
+//         console .log ("C")}
+
+//     else if(num> 60 && num <70 ){
+//         console .log ("D")}
+
+               
+//     else if(num> 50 && num <60 ){
+//         console .log ("E")}
+
+//     else if(num <50 &&num <0  ){
+//         console .log ("F")}
